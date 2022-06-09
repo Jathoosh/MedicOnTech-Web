62f2da2d325642d0962f502d0cf6746a464d4ca9
Jathoosh <73128006+Jathoosh@users.noreply.github.com>
2022-06-09 16:24:30 +0200

    Tentative Data_Db

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,18 +1,18 @@
 const express = require('express')
 const router = express.Router()
 
-const Data_Db = require('../.Data_Db')
+const data_db = require('../.data_db')
 //Import lines from .Data_Db file
 
 const {Sequelize} = require('sequelize');
 const { status } = require('express/lib/response');
 
 //Attention la table utilisé ici est db_tp5 (avec articles et users à l'intérieur)
-const sequelize = new Sequelize("db_medicontech",Data_Db.Username,Data_Db.Password, //Veuillez mettre le mot de passe de la base de donnée
+const sequelize = new Sequelize("db_medicontech",data_db.Username,data_db.Password, //Veuillez mettre le mot de passe de la base de donnée
 {
   dialect: "mysql",
   host: "localhost",
-  port: Data_Db.Port // Changer le port si vous utilisez un autre port que 3306
+  port: data_db.Port // Changer le port si vous utilisez un autre port que 3306
 
 });
 
