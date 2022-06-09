942a5cb43b502acb0f13135684a6e715ab6c5b26
Jathoosh <73128006+Jathoosh@users.noreply.github.com>
2022-06-09 16:25:41 +0200

    Update api.js

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-const data_db = require('../.data_db')
-//Import lines from .Data_Db file
+const data_db = require('./.data_db')
 
 const {Sequelize} = require('sequelize');
 const { status } = require('express/lib/response');
