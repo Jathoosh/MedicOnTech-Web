a4f48981c773b10ba43583d1ab80914c495f16d4
Jathoosh <73128006+Jathoosh@users.noreply.github.com>
2022-06-09 14:41:38 +0200

    Suite Initialisation
    Correction Bug Sql
    Création fichier .Data_Db

--- projet.sql
+++ projet.sql
@@ -5,7 +5,7 @@ use db_medicontech;
 /*Create Tables*/
 
 CREATE TABLE Postal_address(
-   Id_Postal_address COUNTER,
+   Id_Postal_address INT NOT NULL AUTO_INCREMENT,
    door_number VARCHAR(50),
    number VARCHAR(50),
    road VARCHAR(100),
@@ -16,7 +16,7 @@ CREATE TABLE Postal_address(
 );
 
 CREATE TABLE Drug(
-   Id_Drug COUNTER,
+   Id_Drug INT NOT NULL AUTO_INCREMENT,
    drug_name VARCHAR(50),
    drug_price DECIMAL(15,2),
    reimbursement_rate DECIMAL(15,2),
@@ -24,25 +24,25 @@ CREATE TABLE Drug(
 );
 
 CREATE TABLE Service(
-   Id_Service COUNTER,
+   Id_Service INT NOT NULL AUTO_INCREMENT,
    service_name VARCHAR(50),
    PRIMARY KEY(Id_Service)
 );
 
 CREATE TABLE Mutual_insurance(
-   Id_Mutual_insurance COUNTER,
+   Id_Mutual_insurance INT NOT NULL AUTO_INCREMENT,
    mutual_name VARCHAR(50),
    PRIMARY KEY(Id_Mutual_insurance)
 );
 
 CREATE TABLE Speciality(
-   Id_Speciality COUNTER,
+   Id_Speciality INT NOT NULL AUTO_INCREMENT,
    speciality_name VARCHAR(50),
    PRIMARY KEY(Id_Speciality)
 );
 
 CREATE TABLE Person(
-   Id_Person COUNTER,
+   Id_Person INT NOT NULL AUTO_INCREMENT,
    first_name VARCHAR(50) NOT NULL,
    last_name VARCHAR(50) NOT NULL,
    birth_date DATE NOT NULL,
@@ -54,21 +54,21 @@ CREATE TABLE Person(
 );
 
 CREATE TABLE Pharmacist(
-   Id_Pharmacist COUNTER,
+   Id_Pharmacist INT NOT NULL AUTO_INCREMENT,
    Id_Person INT NOT NULL,
    PRIMARY KEY(Id_Pharmacist),
    FOREIGN KEY(Id_Person) REFERENCES Person(Id_Person)
 );
 
 CREATE TABLE Doctor(
-   Id_Doctor COUNTER,
+   Id_Doctor INT NOT NULL AUTO_INCREMENT,
    Id_Person INT NOT NULL,
    PRIMARY KEY(Id_Doctor),
    FOREIGN KEY(Id_Person) REFERENCES Person(Id_Person)
 );
 
 CREATE TABLE Patient(
-   Id_Patient COUNTER,
+   Id_Patient INT NOT NULL AUTO_INCREMENT,
    Id_tutor INT,
    social_security_number VARCHAR(50),
    Id_Mutual_insurance INT,
@@ -79,16 +79,16 @@ CREATE TABLE Patient(
 );
 
 CREATE TABLE Prescription(
-   Id_Prescription COUNTER,
+   Id_Prescription INT NOT NULL AUTO_INCREMENT,
    creation_date DATE,
    expiration_date DATE,
    date_of_use DATE,
    frequency_of_reuse INT,
    number_of_reuses INT,
-   used LOGICAL,
-   validity LOGICAL,
+   used BOOLEAN,
+   validity BOOLEAN,
    note TEXT,
-   reported LOGICAL,
+   reported BOOLEAN,
    report_note TEXT,
    Id_Doctor INT NOT NULL,
    Id_Patient INT NOT NULL,
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,15 +1,18 @@
 const express = require('express')
 const router = express.Router()
 
+const Data_Db = require('../.Data_Db')
+//Import lines from .Data_Db file
+
 const {Sequelize} = require('sequelize');
 const { status } = require('express/lib/response');
 
 //Attention la table utilisé ici est db_tp5 (avec articles et users à l'intérieur)
-const sequelize = new Sequelize("db_MedicOnTech","Machin","MdpOfMachin", //Veuillez mettre le mot de passe de la base de donnée
+const sequelize = new Sequelize("db_medicontech",Data_Db.Username,Data_Db.Password, //Veuillez mettre le mot de passe de la base de donnée
 {
   dialect: "mysql",
   host: "localhost",
-  port: 3307 // Changer le port si vous utilisez un autre port que 3306
+  port: Data_Db.Port // Changer le port si vous utilisez un autre port que 3306
 
 });
 
