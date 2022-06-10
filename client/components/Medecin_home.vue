3d76097801d815ab56aecd48121be4c972d53dac
NDU <61319301+Overshadow75@users.noreply.github.com>
2022-06-10 22:12:04 +0200

    init Medecin_home.vue

---
+++ client/components/Medecin_home.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <p>Home medecin</p>
+    </div>
+</template>
+
+<script>
+module.exports = {
+    name: 'Annexe',
+
+}
+</script>
--- client/vue-application.js
+++ client/vue-application.js
@@ -1,9 +1,11 @@
 const Home = window.httpVueLoader('./components/Home.vue')
 const Annexe = window.httpVueLoader('./components/Annexe.vue')
+const Medecin_home = window.httpVueLoader('./components/Medecin_home.vue')
 
 const routes = [
   { path: '/', component: Home },
-  { path: '/annexe', name:'Annexe', component: Annexe }
+  { path: '/annexe', name:'Annexe', component: Annexe },
+  { path: '/Medecin_home', name:'Medecin', component: Medecin_home }
 ]
 
 const router = new VueRouter({
