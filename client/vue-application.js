8760c2bc79477a071629c8b3ffd7d9a576c21ff1
Jathoosh <73128006+Jathoosh@users.noreply.github.com>
2022-06-11 19:17:07 +0200

    Header Footer terminé (Manque RS)

--- client/vue-application.js
+++ client/vue-application.js
@@ -1,9 +1,11 @@
 const Home = window.httpVueLoader('./components/Home.vue')
 const Annexe = window.httpVueLoader('./components/Annexe.vue')
+const Annexe2 = window.httpVueLoader('./components/Bonchour.vue')
 
 const routes = [
   { path: '/', component: Home },
-  { path: '/annexe', name:'Annexe', component: Annexe }
+  { path: '/annexe', name:'Annexe', component: Annexe },
+  { path: '/annexe2', name:'Annexe2', component: Annexe2 },
 ]
 
 const router = new VueRouter({
