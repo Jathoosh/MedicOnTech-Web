686966323a89c08aaa4b0955f000adde27ae6ee3
NDU <61319301+Overshadow75@users.noreply.github.com>
2022-06-16 15:33:35 +0200

    patient_personna_a_charge

---
+++ client/components/Dependent_patient.vue
@@ -0,0 +1,28 @@
+<template>
+   <div>
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+</div>
+
+</template>
+
+<script>
+module.exports = {
+    name: 'Dependent_patient',
+    data() {
+        return {
+           
+        }
+    },
+
+}
+</script>
+
+<style>
+
+</style>
