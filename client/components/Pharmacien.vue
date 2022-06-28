a06f09f811f86962c1216c01d670780380b93641
lisa-rl <lisa.raoul@efrei.net>
2022-06-28 17:50:59 +0200

    Pharmacien mis à jour
    Reste à : mettre le texte "Cause du signalement" de input en haut du rectangle

--- client/components/Pharmacien.vue
+++ client/components/Pharmacien.vue
@@ -6,32 +6,35 @@
             <button @click="retourPagePrincipale" class="btn text-center" type="submit">Deconnexion</button>
         </div>
         
-        <div class = "rectangle">
-            <p class="title_profil"> Recherche avancée </p>
-        </div>    
+        <div class = "rectangle"> <p class="title_profil"> Recherche avancée </p> </div>    
         
         <div id="container"> 
-            <input class="SearchBar" type="search" v-model="search" placeholder="ID ordonnance" size="28"/>
+            <div>
+                <input class="buttonTextSize SearchBar" type="search" id="search" placeholder="ID ordonnance" size="28"/>
+                <button @click="rechercherID()" id = "buttonRechercher" class="buttonTextSize btn text-center">Rechercher</button>              
+            </div>
         </div>
 
         <!-- affichage de l'ordonnance seulement si la fonction scanPrescription renvoit true -->
-        <p v-if="scanPrescription == true"> Affichage de l'ordonnance </p>
+        <!-- <p v-if="scanPrescription == true"> Affichage de l'ordonnance </p> -->
 
         <div class="buttonValiderOrdonnance">
-            <button @click="validerOrdonnance" class="btn text-center" type="submit">Valider ordonnance</button>
+            <button @click="validerOrdonnance" class="buttonTextSize btn text-center" type="submit">Valider ordonnance</button>
         </div>
 
-        <div>
-            <p class="signalement"> Signalement </p>
-        </div>
+        <p class="signalement"> Signalement </p>
         
-        <div id="container">
-            <input class="Signaler" v-model="signaler" placeholder="Cause du signalement, ... ">
-        </div>
-        
-        <div class="buttonSignaler">
-            <button @click="signalerOrdonnance" class="btn text-center" type="submit">Signaler</button>
-        </div>
+        <div id="containerInput"> 
+            <div id="SignalContainer">
+                <input class="buttonTextPosition Signaler" id="signaler" placeholder="Cause du signalement, ... "> 
+                <div class="buttonSignaler"> 
+                    <button @click="signalerOrdonnance" class="buttonTextSize btn text-center" type="submit">Signaler</button> 
+                </div>
+            </div>
+            
+            
+        </div> 
+      
 
     </div>
 </template>
@@ -42,29 +45,38 @@ module.exports = {
     data(){
         return{
             pharmacist: {
-                id_pharmacist: 1, // on ne doit pas le voir sur la page
-                last_name: 'Raoul',
-                first_name: 'Lisa '            
+                id_pharmacist: 1, 
+                last_name: 'Last Name',
+                first_name: 'First Name '            
             }
         }
     },
     methods: {
-        retourPagePrincipale: function(){
-            this.$emit("retour page principale");
-            this.$router.push("/login");
-        }, 
         infosPharmacist: function(){
             var String = this.pharmacist.first_name + this.pharmacist.last_name;
             return String;
         },
+        retourPagePrincipale: function(){
+            this.$emit("retour page principale");
+            this.$router.push("/login");
+        },
+        rechercherID: function(){
+            var idOrdonnance = document.getElementById("search").value;
+            //sera à modifier pour qu'on puisse récupérer l'id de l'ordonnance => backend
+            alert(idOrdonnance);
+        },
+        validerOrdonnance: function(){
+            //this.$emit("ordonannce validée", this.id_prescription);
+            alert("Ordonnance validée");
+        },
         signalerOrdonnance: function(){
-            //this.$emit("signaler", this.id_prescription);
-            //console.log(this.patient); 
-            alert("Signalement ordonnance");
-        }
+            var idOrdonnanceSignalee = document.getElementById("signaler").value;
+            //sera à modifier pour qu'on puisse récupérer l'id de l'ordonnance => backend
+            alert(idOrdonnanceSignalee);
+        }       
     },
     mounted(){
-        // afficher l'ordonnance du patient en fonction de l'id patient récupéré
+        // afficher l'ordonnance du patient en fonction de son id
         // pour ce faire, il faut récupérer l'id_prescription du patient associé de la table patient
     }
 }
@@ -87,17 +99,21 @@ module.exports = {
     
     .buttonDeconnexion{
         position: absolute;
-        width: fit-content;
         left: 1509px;
         top: 167px;
         background: #D9D9D9;
         border-radius: 20px;
     }
   
-    #container {
+    #container {        
         display: flex;
         flex-direction: row;
-        justify-content: center;
+        justify-content: center;   
+    }
+
+    #containerInput {
+        display: flex;
+        flex-direction: column;
     }
     
     #container .SearchBar {
@@ -105,36 +121,63 @@ module.exports = {
         border-left: none;
         border-right: none;
         margin-bottom: 15px;
-        width: 50%;
+        margin-top: 15px;
+        width: 70%;
     }
 
     .buttonValiderOrdonnance{
         width: fit-content;
         margin-left: 790px;
         background: #D9D9D9;
-        border-radius: 20px;
+        border-radius: 20px;  
     }
 
     .signalement{
         width: fit-content;
         height: 50px;
-        margin-left: 492px;
+        margin-left: 441px;
         font-weight: 800;
         font-size: 25px;
     }
 
     .buttonSignaler{
-        width: fit-content;
-        margin-left: 900px;
         background: #D9D9D9;
         border-radius: 20px;
+        max-width: 100px;
+        align-self: flex-end;
     }
 
-    #container .Signaler{
+    #containerInput .Signaler{
         width: 1019px;
         height: 368px;
         background: rgba(216, 216, 216, 0.5);
         border-radius: 20px;
-        margin-bottom: 30px;
+        margin-bottom: 30px;  
+        position: static;   
     }
+
+    #buttonRechercher{
+        width: fit-content;
+        margin-left: 790px;
+        background: #D9D9D9;
+        border-radius: 20px; 
+        margin-right: -162px;
+        margin-top: -69px;
+    }
+    
+    .buttonTextSize{
+        font-size: 20px;
+    }
+
+    .buttonTextPosition{
+        top: 0px;
+    }
+
+    #SignalContainer{
+        align-self: center;
+        display: flex;
+        flex-direction: column;
+        justify-content: end;        
+    }
+
 </style>
